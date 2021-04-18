a383f1a0f8b2e531d820c7287706af494fc18b1d
asimcode2050 <76836462+asimcode2050@users.noreply.github.com>
2021-04-18 15:21:15 +0500

    Create int_example.c

---
+++ c++/int_example.c
@@ -0,0 +1,16 @@
+// https://youtu.be/e_fdlnrbKP4
+
+#include <stdio.h>
+
+int main(){
+  int x;
+  x = 10;
+  int y;
+  y = 012;
+  int z;
+  z = 0xA;
+printf("Decimal : %d Octal: %o Hex : %X", x,y,z);
+
+
+    return 0;
+}
