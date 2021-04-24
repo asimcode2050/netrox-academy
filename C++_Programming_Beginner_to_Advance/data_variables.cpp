1486cb3e998a40264d1f2455c81fa6b2d50d78f6
asimcode2050 <76836462+asimcode2050@users.noreply.github.com>
2021-04-24 21:23:58 +0500

    Create data_variables.cpp

---
+++ C++_Programming_Beginner_to_Advance/data_variables.cpp
@@ -0,0 +1,26 @@
+// https://youtu.be/Vqna-PyamQw
+#include <iostream>
+using namespace std;
+int main(){
+      /*
+        char 1 byte
+        short 2 byte
+        int 4 byte
+        long 4 or 8 byte
+        float 4 byte
+        double 8 byte
+        long double 8 or 16 byte
+        bool 1 byte
+        */
+        int myInt; // correct
+        int 32Int; // incorrect starts with number 
+        int Int 32;// incorrect (space)
+        int Int@32; // incorrect (special char)
+        int new; // incorrect (reserve keyword)
+        myInt = 20;
+        int x = 100;
+        int y(50);
+        int a =1, b = 2,c =5;
+
+return 0;
+} 
