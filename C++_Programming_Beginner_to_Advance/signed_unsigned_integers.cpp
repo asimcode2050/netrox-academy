65904c301536e7d00dd332e8bd3082c1f4c4445b
asimcode2050 <76836462+asimcode2050@users.noreply.github.com>
2021-04-24 23:56:08 +0500

    Create signed_unsigned_integers.cpp

---
+++ C++_Programming_Beginner_to_Advance/signed_unsigned_integers.cpp
@@ -0,0 +1,17 @@
+// https://youtu.be/fYm8-Y4CWsY
+#include <iostream>
+#include <cstdint>
+using namespace std;
+int main(){
+    signed char myChar = 0; //  -128 to +127
+    signed short myShort = 0; //  -32768 to +32767  
+    signed int myInt = 0; // -2^31 to +2^31-1
+    signed long myLong = 0; // -2^31 to +2^31-1
+
+    unsigned char myUChar = 0; // 0 to 255
+    unsigned short myUShort = 0; // 0 to 65535
+    unsigned int myUInt = 0; // 0 to 2^32-1
+    unsigned long myULong = 0; // 0 to 2^32-1
+    
+    return 0;
+} 
