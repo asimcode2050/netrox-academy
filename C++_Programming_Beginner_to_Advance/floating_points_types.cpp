b1f8f4d51fc20fbbeab1b761f31d6380d53c20d4
asimcode2050 <76836462+asimcode2050@users.noreply.github.com>
2021-04-25 01:04:23 +0500

    Create floating_points_types.cpp

---
+++ C++_Programming_Beginner_to_Advance/floating_points_types.cpp
@@ -0,0 +1,12 @@
+// https://youtu.be/io5Lps3PbqY
+#include <iostream>
+#include <cstdint>
+using namespace std;
+int main(){
+  float myFloat; // 7 digits
+  double myDouble;// 15 digits
+  myFloat = 12345.678; // round 12345.68
+  float myFloat2 = 3e2; // 3 * 10 ^ 2 = 300
+  cout << myFloat2 << endl;
+    return 0;
+} 
