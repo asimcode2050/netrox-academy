b28ac20b5a3d12180412c7ec44fa22d976482724
asimcode2050 <76836462+asimcode2050@users.noreply.github.com>
2021-04-25 02:36:07 +0500

    Create arithmetic_operators.cpp

---
+++ C++_Programming_Beginner_to_Advance/arithmetic_operators.cpp
@@ -0,0 +1,16 @@
+// 
+#include <iostream>
+#include <cstdint>
+using namespace std;
+int main(){
+    int x = 3 + 2; // 5 // addition
+    x = 3 - 2; // 1 // subtraction
+    x = 3 * 2; // 6 // multiplication
+    x = 3 / 2; // 1 // division
+    float m = 3 / (float)2; // 1.5
+    x = 3 % 2; // 1 // modulus (remainder)
+   cout << "M: " << m << endl;
+   cout << "X: " << x <<endl;
+    
+    return 0;
+} 
