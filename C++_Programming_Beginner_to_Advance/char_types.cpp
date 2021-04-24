83ef6faff8356b9e5b3761cd15434b562c0e437d
asimcode2050 <76836462+asimcode2050@users.noreply.github.com>
2021-04-25 01:23:34 +0500

    Create char_types.cpp

---
+++ C++_Programming_Beginner_to_Advance/char_types.cpp
@@ -0,0 +1,15 @@
+// https://youtu.be/fJ2d4VQQG4c
+#include <iostream>
+#include <cstdint>
+using namespace std;
+int main(){
+  char c = 'a';
+  char b = 'c';
+  char y = 'x'; // 120
+  cout << y << endl;
+  int i = y;
+  cout << i << endl;
+  cout << (char)i << endl;
+    
+    return 0;
+} 
