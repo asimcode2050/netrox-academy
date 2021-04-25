dc691cb5863420428bbff77081c85618e125051e
asimcode2050 <76836462+asimcode2050@users.noreply.github.com>
2021-04-25 17:54:18 +0500

    Create comparison_operators.cpp

---
+++ C++_Programming_Beginner_to_Advance/comparison_operators.cpp
@@ -0,0 +1,14 @@
+// https://youtu.be/8zdx2vGlSXo
+#include <iostream>
+using namespace std;
+int main(){
+    bool b = (2 == 3); //false // equal to
+    b = (2 != 3); // true // not equal to
+    b = (2 > 3); // false // greater than
+    b = ( 2 < 3); // true // less than
+    b = (2 >= 3); //false // greater than or equal to
+    b = (2 <= 3); // true // less than or equal to 
+    cout << b << endl;
+
+    return 0;
+} 
