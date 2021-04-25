628453988b10450d084f0eee24e6c7b3e0b57c56
asimcode2050 <76836462+asimcode2050@users.noreply.github.com>
2021-04-25 16:58:03 +0500

    Create assignment_operators.cpp

---
+++ C++_Programming_Beginner_to_Advance/assignment_operators.cpp
@@ -0,0 +1,15 @@
+// https://youtu.be/_4zxz3ugLBg
+#include <iostream>
+using namespace std;
+int main(){
+   int x = 0;
+   
+   x += 5; // x = x + 5;
+   x -= 5; // x = x - 5 ;
+   x *= 5; // x = x * 5;
+   x /= 5; // x= x/5;
+   x %= 5; // x = x%5;
+   
+
+    return 0;
+} 
