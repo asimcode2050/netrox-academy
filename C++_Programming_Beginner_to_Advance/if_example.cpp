eb5fc8a729bc27dd3e4a218a561a4a3d70175bef
asimcode2050 <76836462+asimcode2050@users.noreply.github.com>
2021-05-21 00:53:42 +0500

    Create if_example.cpp

---
+++ C++_Programming_Beginner_to_Advance/if_example.cpp
@@ -0,0 +1,21 @@
+/*
+https://youtu.be/hECT-Tt2Flo
+*/
+#include <iostream>
+using namespace std;
+ int main()
+ {  
+     int x = 20;
+     if(x < 20){
+       cout << x << endl;
+     }
+     else if(x > 20){
+        cout << "X is greater Than 20" << endl;
+     }
+     else{
+         cout << "Values are equal" << endl;
+     }
+   
+   return 0;
+
+ }
